7cb3cd0ddf07d848866d7c3993156425b95fa740
unchaptered <workstation19961002@gmail.com>
2023-11-07 01:25:01 +0900

    Update : Test Case 2 - @Module, @Contoller 재귀 테스트 #2

---
+++ src/module/injector/test_case_2.py
@@ -0,0 +1,48 @@
+# 테스트 케이스 1
+# @Module + @Controller / @Module + @Controller 테스트
+
+# decorators
+from decorators.injectable_decorator import Injectable
+from decorators.controller_decorator import Controller
+from decorators.module_decorator import Module
+
+from decorators.http_decorator import get, post, delete
+
+
+if __name__ == '__main__':
+    
+    @Controller('first')
+    class FirstController():
+        
+        @get('profile')
+        def getFirstProfile(self, request):
+            return 'hello'
+        
+    @Module(controllers=[FirstController])
+    class FirstModule:
+        pass
+    
+    @Controller('second')
+    class SecondController():
+        
+        @get('profile')
+        def getFirstProfile(self, request):
+            return 'hello'
+        
+    @Module(controllers=[SecondController])
+    class SecondsModule:
+        pass
+    
+    # print('=' * 50)
+    # print(FirstModule, SecondsModule)
+    # print('=' * 50)
+    
+    @Module(imports=[FirstModule, SecondsModule])
+    class RootModule:
+        pass
+    
+    # print(dir(RootModule))
+    # print(dir(RootModule._RootModule_FirstModule))
+    # print(dir(RootModule._RootModule_SecondsModule))
+    print(RootModule().convertUrlPatterns())
+    
