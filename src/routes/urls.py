4500469a1c857845702a4fef5e9fba8a507fb378
unchaptered <workstation19961002@gmail.com>
2023-11-06 14:58:53 +0900

    Simplify : folders

--- src/routes/urls.py
+++ src/routes/urls.py
@@ -1,6 +1,6 @@
 from routes.dynamic_router import DynamicRouter
 from routes.sample.sample_controller import SampleController
-from routes.sample.kevin_controller import KevinController
+from routes.kevin.kevin_controller import KevinController
 
 urlpatterns = DynamicRouter(controllers=[
     SampleController,
